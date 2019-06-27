a2c1590dd8ab0813b14115e3c01eb6e28c5b7a21
Thiago <thiagobarros@cjr.org.br>
2019-06-27 16:58:01 -0300

    Criando o model de Evento

---
+++ app/src/main/java/com/example/aplicativomural/Evento.java
@@ -0,0 +1,114 @@
+package com.example.aplicativomural;
+
+import java.util.Date;
+
+public class Evento {
+    private String titulo;
+    private Date data;
+    private String local;
+    private String descricao;
+    private Integer horario;
+    private String IdCategoria;
+    private Integer telefone;
+    private String email;
+    private Boolean reportado;
+    private Integer whatsapp;
+    private Integer telegram;
+    private Boolean periodico;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Integer getHorario() {
+        return horario;
+    }
+
+    public void setHorario(Integer horario) {
+        this.horario = horario;
+    }
+
+    public String getIdCategoria() {
+        return IdCategoria;
+    }
+
+    public void setIdCategoria(String idCategoria) {
+        IdCategoria = idCategoria;
+    }
+
+    public Integer getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(Integer telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Boolean getReportado() {
+        return reportado;
+    }
+
+    public void setReportado(Boolean reportado) {
+        this.reportado = reportado;
+    }
+
+    public Integer getWhatsapp() {
+        return whatsapp;
+    }
+
+    public void setWhatsapp(Integer whatsapp) {
+        this.whatsapp = whatsapp;
+    }
+
+    public Integer getTelegram() {
+        return telegram;
+    }
+
+    public void setTelegram(Integer telegram) {
+        this.telegram = telegram;
+    }
+
+    public Boolean getPeriodico() {
+        return periodico;
+    }
+
+    public void setPeriodico(Boolean periodico) {
+        this.periodico = periodico;
+    }
+}
