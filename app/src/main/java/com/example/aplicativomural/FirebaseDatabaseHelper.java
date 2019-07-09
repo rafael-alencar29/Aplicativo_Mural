92d9faaea0cf35c21143b04e9f6bdf7deee371ab
Thiago <thiagobarros@cjr.org.br>
2019-07-08 21:50:43 -0300

    Criando o FirebaseHelper

---
+++ app/src/main/java/com/example/aplicativomural/FirebaseDatabaseHelper.java
@@ -0,0 +1,11 @@
+package com.example.aplicativomural;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class FirebaseDatabaseHelper {
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mReferenceBooks;
+    
+
+}
